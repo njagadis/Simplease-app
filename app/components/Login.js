e26f233ec25440c85412a43a008fa3609efb1f5e
rohanrao35 <31747010+rohanrao35@users.noreply.github.com>
2017-10-06 03:40:15 -0400

    Update Login.js

--- app/components/Login.js
+++ app/components/Login.js
@@ -55,7 +55,7 @@ export default class Login extends Component {
           <View style={styles.logoContainer}>
             <Image
               style={styles.logo}
-              source={require('../../Logo/d.png')}
+              source={require('../img/logo/d.png')}
               />
 
           </View>
